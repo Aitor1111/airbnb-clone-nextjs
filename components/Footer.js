27cce62f6c1b3d92f547038c21b2c15c14d5622c
Aitor Trujillo <zenji@MacBook-Air-de-Aitor.local>
2021-12-02 22:11:05 +0100

    homescreen

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Made by</p>
+                <p>Aitor</p>
+                <p>Trujillo</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Become a Host</p>
+                <p>How to be Host</p>
+                <p>Visit community</p>
+                <p>AirCover</p>
+                <p>Host resources</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>COVID Responsability</p>
+                <p>Safety Info</p>
+                <p>How to cancel</p>
+                <p>Nearby</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
